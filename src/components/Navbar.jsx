f168965455dd29a57bd5ad3a7d03e4c7dd15ac6c
Ebikeme <ebikemeese257@gmail.com>
2026-01-21 06:49:34 +0100

    Add fix website footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                         <div className="pl-5">
                         <Link to="/" className="flex items-center gap-2.5">
                             <ShipWheelIcon className="size-9 text-primary" />
-                            <span className="text-3xl sm:hidden font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+                            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
                             Streamify
                             </span>
                         </Link>
@@ -28,11 +28,11 @@ const Navbar = () => {
                     )}
 
                     <div className="flex items-center gap-3 sm:gap-4 ml-auto">
-                        {/* <Link to={"/"}>
+                        <Link to={"/"}>
                             <button className="btn btn-ghost btn-circle">
                                 <HomeIcon className="h-6 w-6 text-base-content opacity-70" />
                             </button>
-                        </Link> */}
+                        </Link>
 
                         <Link to={"/friends"}>
                             <button className="btn btn-ghost btn-circle">
